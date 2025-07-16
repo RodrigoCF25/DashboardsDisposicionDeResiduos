a0fa7d3441ab789745ba674d86a3bd1707d265c5
RodrigoCF25 <rodrigocf88@hotmail.com>
2025-07-15 18:07:40 -0600

    Defined styles

---
+++ dashboards/src/assets/colors.js
@@ -0,0 +1,12 @@
+const COLORS = [
+  "#5BC0FF", // blue light
+  "#843ADD", // purple
+  "#5300B8", // blue deep
+  "#2A8AD9", // blue medium
+  "#8840A7", // purple dark
+  "#56A4DD", // blue soft
+  "#3961A8", // blue dark
+];
+
+
+export {COLORS}
