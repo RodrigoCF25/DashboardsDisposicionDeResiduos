b5864b9ef62f14d1ce41964d5595e3cd952e5ad9
RodrigoCF25 <rodrigocf88@hotmail.com>
2025-07-15 18:14:22 -0600

    Expanded data for dashboards

--- dashboards/src/data/recoleccionData.js
+++ dashboards/src/data/recoleccionData.js
@@ -1,21 +1,47 @@
 export const recoleccionData = [
-    {
-    cliente: "Enerwaste Norte",
-    mes: "Junio",
-    toneladas: 15,
-    km: 50,
-    litros: 35,
-    horas_activas: 5,
-    horas_muertas: 1,
 
-    },
-    {
-    cliente: "Enerwaste Sur",
-    mes: "Junio",
-    toneladas: 12,
-    km: 42,
-    litros: 30,
-    horas_activas: 4,
-    horas_muertas: 0.5,
-    }
+  { cliente: "Enerwaste Norte", mes: "Marzo", toneladas: 13, km: 48, litros: 30, horas_activas: 4.5, horas_muertas: 0.9 },
+  { cliente: "Enerwaste Norte", mes: "Abril", toneladas: 14, km: 52, litros: 32, horas_activas: 5, horas_muertas: 1 },
+  { cliente: "Enerwaste Norte", mes: "Mayo", toneladas: 16, km: 54, litros: 34, horas_activas: 5.5, horas_muertas: 1 },
+  { cliente: "Enerwaste Norte", mes: "Junio", toneladas: 15, km: 50, litros: 35, horas_activas: 5, horas_muertas: 1 },
+  { cliente: "Enerwaste Norte", mes: "Julio", toneladas: 18, km: 55, litros: 38, horas_activas: 6, horas_muertas: 1.2 },
+
+
+  { cliente: "Enerwaste Sur", mes: "Abril", toneladas: 11, km: 39, litros: 27, horas_activas: 3.5, horas_muertas: 0.4 },
+  { cliente: "Enerwaste Sur", mes: "Mayo", toneladas: 13, km: 41, litros: 29, horas_activas: 3.8, horas_muertas: 0.5 },
+  { cliente: "Enerwaste Sur", mes: "Junio", toneladas: 12, km: 42, litros: 30, horas_activas: 4, horas_muertas: 0.5 },
+  { cliente: "Enerwaste Sur", mes: "Julio", toneladas: 10, km: 40, litros: 28, horas_activas: 3.8, horas_muertas: 0.6 },
+
+
+  { cliente: "BioResiduos", mes: "Marzo", toneladas: 19, km: 59, litros: 41, horas_activas: 6.2, horas_muertas: 0.7 },
+  { cliente: "BioResiduos", mes: "Abril", toneladas: 21, km: 61, litros: 43, horas_activas: 6.5, horas_muertas: 0.9 },
+  { cliente: "BioResiduos", mes: "Mayo", toneladas: 17, km: 58, litros: 40, horas_activas: 6, horas_muertas: 1 },
+  { cliente: "BioResiduos", mes: "Junio", toneladas: 20, km: 60, litros: 42, horas_activas: 6.5, horas_muertas: 0.8 },
+
+
+  { cliente: "EcoServicios", mes: "Marzo", toneladas: 12, km: 40, litros: 28, horas_activas: 4, horas_muertas: 0.5 },
+  { cliente: "EcoServicios", mes: "Abril", toneladas: 14, km: 45, litros: 32, horas_activas: 4.5, horas_muertas: 0.7 },
+  { cliente: "EcoServicios", mes: "Mayo", toneladas: 16, km: 48, litros: 34, horas_activas: 5, horas_muertas: 0.9 },
+
+
+  { cliente: "ReciPlanta", mes: "Abril", toneladas: 10, km: 36, litros: 26, horas_activas: 3.2, horas_muertas: 0.4 },
+  { cliente: "ReciPlanta", mes: "Mayo", toneladas: 12, km: 37, litros: 28, horas_activas: 3.7, horas_muertas: 0.5 },
+  { cliente: "ReciPlanta", mes: "Junio", toneladas: 11, km: 38, litros: 29, horas_activas: 3.5, horas_muertas: 0.4 },
+  { cliente: "ReciPlanta", mes: "Julio", toneladas: 13, km: 40, litros: 31, horas_activas: 4, horas_muertas: 0.6 },
+
+
+  { cliente: "GreenCollect", mes: "Marzo", toneladas: 17, km: 50, litros: 34, horas_activas: 5, horas_muertas: 0.9 },
+  { cliente: "GreenCollect", mes: "Abril", toneladas: 21, km: 54, litros: 37, horas_activas: 6, horas_muertas: 1.3 },
+  { cliente: "GreenCollect", mes: "Mayo", toneladas: 19, km: 52, litros: 36, horas_activas: 5.5, horas_muertas: 1.1 },
+
+
+  { cliente: "Soluciones Ambientales", mes: "Marzo", toneladas: 22, km: 59, litros: 39, horas_activas: 6.2, horas_muertas: 0.9 },
+  { cliente: "Soluciones Ambientales", mes: "Abril", toneladas: 24, km: 62, litros: 41, horas_activas: 6.7, horas_muertas: 1 },
+  { cliente: "Soluciones Ambientales", mes: "Mayo", toneladas: 23, km: 60, litros: 40, horas_activas: 6.5, horas_muertas: 1 },
+
+
+  { cliente: "Tratamientos ECO", mes: "Abril", toneladas: 13, km: 42, litros: 30, horas_activas: 4.2, horas_muertas: 0.6 },
+  { cliente: "Tratamientos ECO", mes: "Mayo", toneladas: 15, km: 44, litros: 32, horas_activas: 4.6, horas_muertas: 0.7 },
+  { cliente: "Tratamientos ECO", mes: "Junio", toneladas: 14, km: 44, litros: 33, horas_activas: 4.8, horas_muertas: 0.6 },
+  { cliente: "Tratamientos ECO", mes: "Julio", toneladas: 16, km: 46, litros: 34, horas_activas: 5.1, horas_muertas: 0.7 }
 ];
